f837a535eeda71c2d3eb5ab5502b1479381f73c5
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-02-21 19:19:12 +0100

    Destroy collections when user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :collections, dependent: :destroy
   has_secure_password
   validates :login, :password, presence: true, on: :create
   validates :login, uniqueness: true
