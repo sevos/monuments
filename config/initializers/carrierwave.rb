3f29f0b7b8e7e6333ec6bcc90eb984253b97395c
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-02-21 23:57:24 +0100

    Mock uploads in tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV.fetch('AWS_SECRET_ACCESS_KEY'),       # required
     :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'sevos-monuments' # required
+  config.fog_directory  = ENV.fetch('AWS_FOG_DIRECTORY') # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
   # for more optional configuration
 end
